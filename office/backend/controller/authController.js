c9e836da4845516b4572282e17430ff73191ee82
iamanzalshaikh <tocxanzalftw49@gmail.com>
2025-09-09 20:26:22 +0530

    Update authController.js

--- office/backend/controller/authController.js
+++ office/backend/controller/authController.js
@@ -74,11 +74,11 @@ export const login = async (req, res) => {
 // LOGOUT
 export const logout = (req, res) => {
   try {
-   res.clearCookie("token", {
+ res.clearCookie("token", {
   httpOnly: true,
-  sameSite: "strict",  // match what you used in production
-  secure: true,        // must be true if cookie was set with HTTPS
-  path: "/",           // same path as when cookie was set
+  sameSite: "None",  // must match login/signup
+  secure: true,
+  path: "/",
 });
     res.status(200).json({ message: "Logout successful" });
   } catch (error) {
