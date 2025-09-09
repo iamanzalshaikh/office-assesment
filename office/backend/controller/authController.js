264900c4ced78b0640387f8e9b5d59cfe28940d1
iamanzalshaikh <tocxanzalftw49@gmail.com>
2025-09-09 20:27:03 +0530

    Update authController.js

--- office/backend/controller/authController.js
+++ office/backend/controller/authController.js
@@ -55,9 +55,9 @@ export const login = async (req, res) => {
 
     const token = genToken(user._id);
 
-   res.cookie("token", token, {
+res.cookie("token", token, {
   httpOnly: true,             // prevents JS access (good!)
-  sameSite: "strict",         // stronger CSRF protection
+sameSite: "None",
   secure: true,               // must be true for HTTPS
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 });
