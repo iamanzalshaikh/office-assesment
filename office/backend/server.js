bc47e4fabab69ef71de813d4a36761d7043703dc
iamanzalshaikh <tocxanzalftw49@gmail.com>
2025-09-09 19:53:46 +0530

    Update server.js

--- office/backend/server.js
+++ office/backend/server.js
@@ -22,9 +22,10 @@ const PORT = process.env.PORT || 3000;
 
 // ✅ CORS configuration
 app.use(cors({
-    origin: "http://localhost:5173", 
-    credentials: true,              
+  origin: "https://officemanagment.netlify.app", 
+  credentials: true,                            
 }));
+
 
 // Middleware
 app.use(express.json());
